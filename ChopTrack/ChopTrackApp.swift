0b073933d3ccb810d8cee0eddee17e18056c2036
pimbo <pimboto@hotmail.com>
2025-05-30 15:14:41 -0500

    Initial Commit

---
+++ ChopTrack/ChopTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopTrackApp.swift
+//  ChopTrack
+//
+//  Created by TinderOP on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct ChopTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChopTrackTests/ChopTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChopTrackTests.swift
+//  ChopTrackTests
+//
+//  Created by TinderOP on 5/30/25.
+//
+
+import Testing
+@testable import ChopTrack
+
+struct ChopTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
